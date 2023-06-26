dbad53c134cdea57e366540f9fe4a325a9057732
Lassi Chen <lachen@utu.fi>
2023-06-26 06:43:52 +0300

    0.1.0: Add ability load and save to a file

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 todolist = []
 
+# Load list from file
+with open("ToDo.txt") as file:
+    for entry in file:
+        todolist.append(entry.strip())
+
+# Make entries
 print("Make your own To Do List:")
 while True:
     entry = input("Give an entry (end with empty string): ")
@@ -10,8 +16,14 @@ while True:
     print(f"Added {entry} in the list")
 print("")
 
+# Print list
 print("Your own To Do List:")
-index = 1
-for entry in todolist:
-    print(f"{index}. {entry}")
-    index += 1
+for index, entry in enumerate(todolist):
+    print(f"{index + 1}. {entry}")
+
+# Save to file
+with open("ToDo.txt", "w") as file:
+    for index, entry in enumerate(todolist):
+        file.write(entry)
+        if index + 1 != len(todolist):
+            file.write("\n")
