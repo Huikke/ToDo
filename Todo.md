d5cc8decf747520a045a6b9563ad62c3880173f1
Lassi Chen <lachen@utu.fi>
2023-06-27 17:43:49 +0300

    0.2.0: Add UI

--- Todo.md
+++ Todo.md
@@ -3,5 +3,5 @@
 - [ ] [ ] function
 - [ ] ability to edit
 - [ ] Jsonfy
-- [ ] UI
+- [x] UI
 - [ ] GUI
--- main.py
+++ main.py
@@ -6,20 +6,52 @@ with open("ToDo.txt") as file:
         todolist.append(entry.strip())
 
 # Make entries
-print("Make your own To Do List:")
-while True:
-    entry = input("Give an entry (end with empty string): ")
-    if entry == "":
-        break
-    
+def add_entry(title):
+    entry = title
     todolist.append(entry)
     print(f"Added {entry} in the list")
-print("")
 
 # Print list
-print("Your own To Do List:")
-for index, entry in enumerate(todolist):
-    print(f"{index + 1}. {entry}")
+def display():
+    print("Your To Do list:")
+    for index, entry in enumerate(todolist):
+        print(f"{index + 1}. {entry}")
+
+# Print UI
+def ui():
+    print("Choose action:")
+    print("1. Add task")
+    print("2. Complete task")
+    print("3. Delete task")
+    print("4. Print To Do list")
+    print("5. End program & save to file")
+
+# Main loop
+first = True
+while True:
+    if first:
+        display()
+        print()
+        first = False
+    ui()
+
+    choice = input("Input: ")
+    if choice == "1":
+        add_entry(input("Task title: "))
+    elif choice == "2":
+        #TODO
+        print("WIP")
+    elif choice == "3":
+        #TODO
+        print("WIP")
+    elif choice == "4":
+        print()
+        display()
+    elif choice == "5":
+        break
+    else:
+        print("Invalid Input!")
+    print()
 
 # Save to file
 with open("ToDo.txt", "w") as file:
