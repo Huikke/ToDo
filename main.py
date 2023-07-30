5cdefbc1938fe91abfe8828afd724d9f85eebaae
Lassi Chen <lachen@utu.fi>
2023-07-30 04:16:29 +0300

    main.py functions can be used elsewhere

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import json
 from datetime import datetime, timezone
 
-# Load list from file
-with open("ToDo.json") as file:
-    data = json.load(file)
-    todolist = [e for e in data]
+# Load list from a file
+# Returns loaded list
+def load_from_file():
+    with open("ToDo.json") as file:
+        data = json.load(file)
+        todo_list = [e for e in data]
+
+    return todo_list
+
+# Save list to a file
+def save_to_file():
+    with open("ToDo.json", "w") as file:
+        data = json.dumps(todo_list, indent=4)
+        file.write(data)
 
 # Make an entry
 def add_entry(title):
@@ -15,30 +25,30 @@ def add_entry(title):
         "completion_time": None
     }
 
-    todolist.append(entry)
+    todo_list.append(entry)
     print(f"Added '{entry['title']}' in the list")
 
 # Change an entry status
 def change_status(index):
-    if todolist[index]["state"] == False:
-        todolist[index]["state"] = True
-        todolist[index]["completion_time"] = datetime.now(timezone.utc).isoformat(timespec="seconds")
-        print(f"'{todolist[index]['title']}' completed!")
+    if todo_list[index]["state"] == False:
+        todo_list[index]["state"] = True
+        todo_list[index]["completion_time"] = datetime.now(timezone.utc).isoformat(timespec="seconds")
+        print(f"'{todo_list[index]['title']}' completed!")
     else:
-        todolist[index]["state"] = False
-        todolist[index]["completion_time"] = None
-        print(f"'{todolist[index]['title']}' completion reverted")
+        todo_list[index]["state"] = False
+        todo_list[index]["completion_time"] = None
+        print(f"'{todo_list[index]['title']}' completion reverted")
 
 # Change an entry title
 def change_title(index, new_title):
-    old_title = todolist[index]["title"]
-    todolist[index]["title"] = new_title
+    old_title = todo_list[index]["title"]
+    todo_list[index]["title"] = new_title
     print(f"'{old_title}' changed to '{new_title}'")
 
 # Delete an entry
 def delete_entry(index):
-    print(f"Deleted '{todolist[index]['title']}' from the list")
-    del todolist[index]
+    print(f"Deleted '{todo_list[index]['title']}' from the list")
+    del todo_list[index]
 
 # Timezone offset
 def timezone_convert(date):
@@ -47,18 +57,18 @@ def timezone_convert(date):
 
 # View timeline
 def entry_timeline(index):
-    print(f"'{todolist[index]['title']}' timeline:")
-    creation_time = timezone_convert(datetime.fromisoformat(todolist[index]["creation_time"])).strftime("%d/%Y/%m %H:%M")
+    print(f"'{todo_list[index]['title']}' timeline:")
+    creation_time = timezone_convert(datetime.fromisoformat(todo_list[index]["creation_time"])).strftime("%d/%Y/%m %H:%M")
     print(f"Created: {creation_time}")
 
-    if todolist[index]['completion_time'] != None:
-        completion_time = timezone_convert(datetime.fromisoformat(todolist[index]["completion_time"])).strftime("%d/%Y/%m %H:%M")
+    if todo_list[index]['completion_time'] != None:
+        completion_time = timezone_convert(datetime.fromisoformat(todo_list[index]["completion_time"])).strftime("%d/%Y/%m %H:%M")
         print(f"Completed: {completion_time}")
 
 # Print list
 def display():
     print("Your To Do list:")
-    for index, entry in enumerate(todolist):
+    for index, entry in enumerate(todo_list):
         checkmark = "x" if entry['state'] == True else " "
         print(f"{index + 1}. [{checkmark}] {entry['title']}")
 
@@ -73,36 +83,35 @@ def ui():
     print("6. View task timeline")
     print("0. End program & save to file")
 
-# Main loop
-first = True
-while True:
-    if first:
-        display()
-        print()
-        first = False
-    ui()
 
-    choice = input("Input: ")
-    if choice == "1":
-        add_entry(input("Task title: "))
-    elif choice == "2":
-        change_status(int(input("Entry index: ")) - 1)
-    elif choice == "3":
-        change_title(int(input("Entry index: ")) - 1, input("New title: "))
-    elif choice == "4":
-        delete_entry(int(input("Delete entry index: ")) - 1)
-    elif choice == "5":
-        print()
-        display()
-    elif choice == "6":
-        entry_timeline(int(input("Entry index: ")) - 1)
-    elif choice == "0":
-        break
-    else:
-        print("Invalid Input!")
-    print()
+if __name__ == "__main__":
+    # Main loop
+    first = True
+    while True:
+        if first:
+            todo_list = load_from_file()
+            display()
+            print()
+            first = False
+        ui()
 
-# Save to file
-with open("ToDo.json", "w") as file:
-    data = json.dumps(todolist, indent=4)
-    file.write(data)
+        choice = input("Input: ")
+        if choice == "1":
+            add_entry(input("Task title: "))
+        elif choice == "2":
+            change_status(int(input("Entry index: ")) - 1)
+        elif choice == "3":
+            change_title(int(input("Entry index: ")) - 1, input("New title: "))
+        elif choice == "4":
+            delete_entry(int(input("Delete entry index: ")) - 1)
+        elif choice == "5":
+            print()
+            display()
+        elif choice == "6":
+            entry_timeline(int(input("Entry index: ")) - 1)
+        elif choice == "0":
+            save_to_file()
+            break
+        else:
+            print("Invalid Input!")
+        print()
